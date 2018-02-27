726223b10d17c1bf66eebd24d7fe9cab65c15488
Takayuki Shimada <taka@tsmd.jp>
2018-02-27 17:06:40 +0900

    Add enable and disable feature

--- docs/example/index.ts
+++ docs/example/index.ts
@@ -4,4 +4,12 @@ const element = document.querySelector('section')
 const scopedScroll = new ScopedScroll(element)
 scopedScroll.init()
 
+document.querySelector('#enable').addEventListener('click', () => {
+  scopedScroll.enable()
+})
+
+document.querySelector('#disable').addEventListener('click', () => {
+  scopedScroll.disable()
+})
+
 console.log('scoped', scopedScroll)
--- src/scoped-scroll.ts
+++ src/scoped-scroll.ts
@@ -1,6 +1,7 @@
 import { throttle } from 'underscore'
 
 export class ScopedScroll {
+  private disabled = false
 
   private document: Document
   private window: Window
@@ -23,9 +24,8 @@ export class ScopedScroll {
   }
 
   init () {
-    if ('overscrollBehavior' in this.element.style) {
-      const elementStyle = this.element.style as any
-      elementStyle.overscrollBehavior = 'contain'
+    if (this.isOverscrollBehaviorSupported()) {
+      this.setOverscrollBehavior('contain')
       return
     }
 
@@ -73,11 +73,36 @@ export class ScopedScroll {
     this.clientHeight = this.element.clientHeight
   }
 
+  enable () {
+    this.disabled = false
+    this.setOverscrollBehavior('contain')
+  }
+
+  disable () {
+    this.disabled = true
+    this.setOverscrollBehavior('')
+  }
+
+  isOverscrollBehaviorSupported () {
+    return 'overscrollBehavior' in this.element.style
+  }
+
+  setOverscrollBehavior (value: string) {
+    if (this.isOverscrollBehaviorSupported()) {
+      const elementStyle = this.element.style as any
+      elementStyle.overscrollBehavior = value
+    }
+  }
+
   private _onResize () {
     this.refreshMetrics()
   }
 
   private _onWheel (e: WheelEvent) {
+    if (this.disabled) {
+      return
+    }
+
     const scrollTop = this.element.scrollTop
     const isTop = scrollTop === 0
     const isBottom = scrollTop >= this.scrollHeight! - this.clientHeight!
@@ -87,12 +112,20 @@ export class ScopedScroll {
   }
 
   private _onTouchStart (e: TouchEvent) {
+    if (this.disabled) {
+      return
+    }
+
     if (e.targetTouches.length === 1) {
       this.clientY = e.targetTouches[0].clientY
     }
   }
 
   private _onTouchMove (e: TouchEvent) {
+    if (this.disabled) {
+      return
+    }
+
     if (e.targetTouches.length === 1) {
       const clientY = e.targetTouches[0].clientY - this.clientY!
       const scrollTop = this.element.scrollTop
@@ -103,5 +136,4 @@ export class ScopedScroll {
       }
     }
   }
-
 }
